69003f4003efa90c3de7e3cc570aa47200fd8dc9
Connor Burns <ctrekker4@gmail.com>
2020-03-24 09:17:30 -0700

    Added discord bot functionality for QuarantineCraft

--- client/run.js
+++ client/run.js
@@ -1,30 +1,49 @@
 var fs=require('fs');
 var request=require('request');
-var externalip=require('externalip');
+var publicIp=require('public-ip');
 
-var baseUrl = 'http://ctrekker.mjtrekkers.com/utilities/DynamicIpTracker/';
-var computerName = 'NONE';
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-externalip(function(err, ip) {
-    if(err) throw err;
-    try {
-        if(fs.readFileSync('ip.txt').toString()!==ip) {
-            fs.appendFileSync('changes.txt', getTimeStamp()+' -> '+ip);
-        }
-    } catch(err) {};
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.channels.fetch('690700442866286612').then(channel => {
+        console.log('Fetched '+channel.name)
 
-    fs.writeFileSync('ip.txt', ip);
-    
-    request.post(
-        baseUrl + 'update.php?name=' + computerName + '&ip='+ip+'&key=GgYUJRXrAWJpdtICCDsq',
-        {},
-        function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                fs.writeFileSync('last_success.txt', getTimeStamp());
-            }
-        }
-    );
+        performIpCheck(channel);
+    }).catch(console.error);
 });
+
+client.login('NjkyMDIwOTI3NjA4NzgyODU4.XnodNA.rQfukWGTF1KDI8BwYKC_YPV77oo');
+
+function performIpCheck(announceChannel) {
+    var baseUrl = 'http://ctrekker.mjtrekkers.com/utilities/DynamicIpTracker/';
+    var computerName = 'NONE';
+
+    publicIp.v4().then(function(ip) {
+        try {
+            if(fs.readFileSync('ip.txt').toString()!==ip) {
+                fs.appendFileSync('changes.txt', getTimeStamp()+' -> '+ip + '\n');
+                announceChannel.send('The server IP has changed to ' + ip);
+            }
+        } catch(err) {};
+
+        fs.writeFileSync('ip.txt', ip);
+
+        request.post(
+            baseUrl + 'update.php?name=' + computerName + '&ip='+ip+'&key=GgYUJRXrAWJpdtICCDsq',
+            {},
+            function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    fs.writeFileSync('last_success.txt', getTimeStamp());
+                    client.destroy();
+                }
+            }
+        );
+    }).catch(function(err) {
+        console.log(err);
+    });
+}
 
 function getTimeStamp() {
     var now = new Date();
